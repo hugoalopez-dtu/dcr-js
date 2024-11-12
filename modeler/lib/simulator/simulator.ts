be10c8b3fdd23ab3e22776c1a7ef899b002e70c1
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2024-11-12 12:13:40 +0100

    Changed structure for simulator and added support for nesting blocks in simulation

--- modeler/lib/simulator/simulator.ts
+++ modeler/lib/simulator/simulator.ts
@@ -1,20 +1,20 @@
 import {
     DCRGraph,
     Event,
-    Marking,
+    EventMap
 } from "./types";
 
 import { execute, isEnabled } from "./align";
+import { clear } from "min-dom";
 
-let graph: DCRGraph;
+let dcrGraph: DCRGraph;
+let events: Set<any>;
+let relations: Set<any>;
+let nestings: Set<any>;
+let subProcesses: Set<any>;
 
-export const executeEvent  = (event: Event) => {
-    if (!isEnabled(event, graph)) return;
-    execute(event, graph);
-}
-
-export const startSimulator = (elementReg: any) => {
-    const dcrGraph: DCRGraph = {
+const clearGraph = () => {
+    dcrGraph = {
         events: new Set(),
         conditionsFor: {},
         milestonesFor: {},
@@ -27,8 +27,23 @@ export const startSimulator = (elementReg: any) => {
             pending: new Set(),
         },
     }
+    
+    events = new Set();
+    relations = new Set();
+    nestings = new Set();
+    subProcesses = new Set();
+}
+
+export const executeEvent  = (event: Event) => {
+    if (!isEnabled(event, dcrGraph)) return;
+    execute(event, dcrGraph);
+}
+
+export const startSimulator = (elementReg: any) => {
+    clearGraph();
+
     elementReg.forEach((element: any) => {
-        if (element.type === 'dcr:Event') {
+        if (element.type === 'dcr:Event' ||element.type === 'dcr:SubProcess') {
             dcrGraph.events.add(element.id);
             dcrGraph.conditionsFor[element.id] = new Set();
             dcrGraph.milestonesFor[element.id] = new Set();
@@ -36,53 +51,85 @@ export const startSimulator = (elementReg: any) => {
             dcrGraph.includesTo[element.id] = new Set();
             dcrGraph.excludesTo[element.id] = new Set();
             dcrGraph.marking.included.add(element.id);
-            /*if (element.businessObject.get('pending')) {
-                dcrGraph.marking.pending.add(element.id);
-            }
-            if (element.businessObject.get('included')) {
-                dcrGraph.marking.included.add(element.id);
-            }
-            if (element.businessObject.get('executed')) {
-                dcrGraph.marking.executed.add(element.id);
-            }*/
+        }
+        switch (element.type) {
+            case 'dcr:Event':
+                events.add(element);
+                break;
+            case 'dcr:Relation':
+                relations.add(element);
+                break;
+            case 'dcr:Nesting':
+                nestings.add(element);
+                break;
+            case 'dcr:SubProcess':
+                subProcesses.add(element);
+                break;
         }
     });
-    elementReg.forEach((element: any) => {
-        if (element.type === 'dcr:Relation') {
-            switch (element.businessObject.get('type')) {
-                case 'condition':
-                    dcrGraph.conditionsFor[element.businessObject.get('targetRef').id]
-                        .add(element.businessObject.get('sourceRef').id);
-                    break;
-                case 'milestone':
-                    dcrGraph.milestonesFor[element.businessObject.get('targetRef').id]
-                        .add(element.businessObject.get('sourceRef').id);
-                    break;
-                case 'response':
-                    dcrGraph.responseTo[element.businessObject.get('sourceRef').id]
-                        .add(element.businessObject.get('targetRef').id);
-                    break;
-                case 'include':
-                    dcrGraph.includesTo[element.businessObject.get('sourceRef').id]
-                        .add(element.businessObject.get('targetRef').id);
-                    break;
-                case 'exclude':
-                    dcrGraph.excludesTo[element.businessObject.get('sourceRef').id]
-                        .add(element.businessObject.get('targetRef').id);
-                    break;
-            }
+    relations.forEach((element: any) => {
+        switch (element.businessObject.get('type')) {
+            case 'condition':
+                addRelation(dcrGraph.conditionsFor,
+                    element.businessObject.get('targetRef' ).id,
+                    element.businessObject.get('sourceRef' ).id);
+                break;
+            case 'milestone':
+                addRelation(dcrGraph.milestonesFor,
+                    element.businessObject.get('targetRef' ).id,
+                    element.businessObject.get('sourceRef' ).id);
+                break;
+            case 'response':
+                addRelation(dcrGraph.responseTo,
+                    element.businessObject.get('sourceRef' ).id,
+                    element.businessObject.get('targetRef' ).id);
+                break;
+            case 'include':
+                addRelation(dcrGraph.includesTo,
+                    element.businessObject.get('sourceRef' ).id,
+                    element.businessObject.get('targetRef' ).id);
+                break;
+            case 'exclude':
+                addRelation(dcrGraph.excludesTo,
+                    element.businessObject.get('sourceRef' ).id,
+                    element.businessObject.get('targetRef' ).id);
+                break;
         }
     });
-    graph = dcrGraph;
+}
+
+const addRelation =
+    (relationSet: EventMap, source: string, target: string) => {
+    if (source.includes('Nesting')) {
+        nestings.forEach((element: any) => {
+            if (element.id === source) {
+                element.children.forEach((nestedElement: any) => {
+                    addRelation(relationSet, nestedElement.id, target);
+                });
+            }
+        });
+    }
+    if (target.includes('Nesting')) {
+        nestings.forEach((element: any) => {
+            if (element.id === target) {
+                element.children.forEach((nestedElement: any) => {
+                    addRelation(relationSet, source, nestedElement.id);
+                });
+            }
+        });
+    }
+
+    if (!source.includes('Nesting') && !target.includes('Nesting')) 
+        relationSet[source].add(target);
 }
 
 export const updateGraph = (elementReg: any, modeler: any) => {
     elementReg.forEach((element: any) => {
         if (element.type === 'dcr:Event') {
-            modeler.get('modeling').updateProperties(element, {executed: graph.marking.executed.has(element.id)});
-            modeler.get('modeling').updateProperties(element, {included: graph.marking.included.has(element.id)});
-            modeler.get('modeling').updateProperties(element, {pending: graph.marking.pending.has(element.id)});
-            modeler.get('modeling').updateProperties(element, {enabled: isEnabled(element.id, graph)});
+            modeler.get('modeling').updateProperties(element, {executed: dcrGraph.marking.executed.has(element.id)});
+            modeler.get('modeling').updateProperties(element, {included: dcrGraph.marking.included.has(element.id)});
+            modeler.get('modeling').updateProperties(element, {pending: dcrGraph.marking.pending.has(element.id)});
+            modeler.get('modeling').updateProperties(element, {enabled: isEnabled(element.id, dcrGraph)});
         }
     });
 }
