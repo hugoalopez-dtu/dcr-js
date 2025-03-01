cff3649d4b1d319d08c6bdadde607df3d6ba20bd
Axel Christfort <axel@christfort.dk>
2025-03-01 19:46:05 +0100

    added import feature

--- app/src/components/Modeler.tsx
+++ app/src/components/Modeler.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import DCRModeler from "modeler";
 
 interface ModelerProps {
--- app/src/components/ModelerState.tsx
+++ app/src/components/ModelerState.tsx
@@ -18,7 +18,7 @@ const BottomButtons = styled.div`
   padding: 1em;
   display: grid;
   grid-template-rows: 1fr 1fr;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   gap: 0.5em;
 `
 const ButtonTitle = styled.div`
@@ -29,6 +29,18 @@ const ButtonTitle = styled.div`
 
 const ModelerState = ({ setState }: StateProps) => {
   const [modeler, setModeler] = useState<DCRModeler>(new DCRModeler({}));
+  const [xml, setXml] = useState<string>(sampleBoardXML);
+
+  // State to reset modeller even without updating the XML.
+  // This is necessary due to the fact that the modeller maintains it's own state, so react doesn't know when to update.
+  // https://www.nikgraf.com/blog/using-reacts-key-attribute-to-remount-a-component
+  const [modelerKey, setModelerKey] = useState<number>(0);
+  const resetModeler = () => setModelerKey(modelerKey + 1);
+
+  const open = (data: string) => () => {
+    setXml(data);
+    resetModeler();
+  }
 
   const saveAsXML = async () => {
     const data = await modeler.saveXML({format: true});
@@ -36,18 +48,22 @@ const ModelerState = ({ setState }: StateProps) => {
     saveAs(blob, "dcr-board.xml"); 
   }
 
+  const saveAsSvg = async () => {
+    const data = await modeler.saveSVG();
+    const blob = new Blob([data.svg]);
+    saveAs(blob, "dcr-board.svg"); 
+  }
+
   return (
     <>
-      <Modeler xml={sampleBoardXML} setModeler={setModeler} />
+      <Modeler key={modelerKey} xml={xml} setModeler={setModeler} />
       <BottomButtons>
         <ButtonTitle>Open:</ButtonTitle>
-        <Button onClick={() => {setState(StateEnum.Test)}}>DCR Solutions XML</Button>
         <Button onClick={() => {}}>Editor XML</Button>
-        <Button onClick={() => {}}>New Diagram</Button>
+        <Button onClick={open(emptyBoardXML)}>New Diagram</Button>
         <ButtonTitle>Download:</ButtonTitle>
         <Button onClick={saveAsXML}>Export XML</Button>
-        <Button onClick={() => {}}>Export XML</Button>
-        <Button onClick={() => {}}>Export XML</Button>
+        <Button onClick={saveAsSvg}>Export SVG</Button>
       </BottomButtons>
     </>
   )
