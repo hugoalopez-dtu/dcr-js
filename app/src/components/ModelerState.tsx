9771448fbc886f8daeb938dec684a848da744e19
Axel Christfort <axel@christfort.dk>
2025-03-18 15:27:34 +0100

    added dropdown tooltip on all states

--- app/src/components/ModelerState.tsx
+++ app/src/components/ModelerState.tsx
@@ -213,7 +213,7 @@ const ModelerState = ({ setState, savedGraphs, setSavedGraphs }: StateProps) => 
       element:
         <MenuElement>
           <DropDown
-            options={[{ title: "TATL2023", value: "TATL2023" }, { title: "HM2011", value: "HM2011" }, { title: "DCR Solutions", value: "DCR Solutions" }]}
+            options={[{ title: "TATL2023", value: "TATL2023", tooltip: "https://orbit.dtu.dk/en/publications/an-open-source-modeling-editor-for-declarative-process-models" }, { title: "HM2011", value: "HM2011", tooltip: "https://arxiv.org/abs/1110.4161" }, { title: "DCR Solutions", value: "DCR Solutions", tooltip: "https://dcrsolutions.net/" }]}
             onChange={(option) => isSettingsVal(option) && modelerRef.current?.setSetting("markerNotation", option)}
           />
           <Label>Relation Notation</Label>
